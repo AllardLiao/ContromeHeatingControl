09eaced2d6215ac31b87016c68b9861597141537
Kai Oey <kai.oey@synergetix.de>
2025-09-16 19:17:54 -0500

    fix

--- libs/ContromeConstants.php
+++ libs/ContromeConstants.php
@@ -110,7 +110,7 @@ class CONTROME_PROFILES
 
     public static function getSetPointPresentation(): string
     {
-        return "['PRESENTATION' => " . GUIDs::PROFILE_SETPOINT_PRESENTATION_ID . ", 'TEMPLATE' => " . GUIDs::PROFILE_SETPOINT_TEMPLATE_ID . "]";
+        return "['PresentationID' => " . GUIDs::PROFILE_SETPOINT_PRESENTATION_ID . ", 'TemplateID' => " . GUIDs::PROFILE_SETPOINT_TEMPLATE_ID . "]";
         //return "~Temperature";
     }
 
