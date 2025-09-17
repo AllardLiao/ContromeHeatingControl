edd1ac78e61586a941f497209d0bbb49f17c12d1
Kai Oey <kai.oey@synergetix.de>
2025-09-17 00:51:26 -0500

    fix

--- ContromeGateway/module.php
+++ ContromeGateway/module.php
@@ -792,7 +792,7 @@ class ContromeGateway extends IPSModuleStrict
         foreach ($rooms as &$room) {
             $room['InstanceExists'] = var_export($this->CheckIfInstanceExists($room['RoomID']), true);
         }
-        $this->SendDebug(__FUNCTION__, "Updated room instance status: " . print_r($rooms), 0);
+        //$this->SendDebug(__FUNCTION__, "Updated room instance status: " . print_r($rooms), 0);
         $this->UpdateFormField("Rooms", "values", json_encode($rooms));
     }
 }
