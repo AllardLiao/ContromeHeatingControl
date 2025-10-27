252207df3610943f6b333ce5e82dafa51e432ec7
Kai Oey <kai.oey@synergetix.de>
2025-10-27 18:05:15 -0500

    test

--- ContromeConfigurator/module.php
+++ ContromeConfigurator/module.php
@@ -163,17 +163,7 @@ class ContromeConfigurator extends IPSModuleStrict
                     'name' => $floorName . ' / ' . $roomName,
                     'FloorID' => $floorId,
                     'RoomID' => $roomId,
-                    'InstanceID' => $instanceID, // 0 = nicht vorhanden, >0 = bereits erstellt
-                    'create' => [
-                        'moduleID' => GUIDs::ROOM_THERMOSTAT,
-                        'name' => 'Thermostat ' . $floorName . ' ' . $roomName,
-                        'configuration' => [
-                            'RoomID' => $roomId,
-                            'FloorID' => $floorId,
-                            'Floor' => $floorName,
-                            'Room' => $roomName
-                        ]
-                    ]
+                    'InstanceID' => $instanceID // 0 = nicht vorhanden, >0 = bereits erstellt
                 ];
             }
         }
