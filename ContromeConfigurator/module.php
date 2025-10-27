044ba3c5c8c81f40b4e59c668805ed759b425f35
Kai Oey <kai.oey@synergetix.de>
2025-10-27 15:04:40 -0500

    fix

--- ContromeConfigurator/module.php
+++ ContromeConfigurator/module.php
@@ -66,8 +66,6 @@ class ContromeConfigurator extends IPSModuleStrict
     public function GetConfigurationForm(): string
     {
         $form = json_decode(file_get_contents(__DIR__ . '/form.json'), true);
-        return json_encode($form);
-
 
         // Raumliste vom Gateway abrufen (gibt JSON-String zurück)
         $responseJson = $this->FetchRoomsFromGateway();
